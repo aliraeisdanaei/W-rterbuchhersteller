41cce8f63656d86e53f0b6d4b7b205b1108ea1c0
Ali Raeisdanaei <mrrookie2@gmail.com>
2020-10-01 08:38:55 -0400

    Made a menu that acts as the interface. Started on the hersteller.py

---
+++ hersteller.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+
+
+# URL = 'https://de.thefreedictionary.com/'
+
+# response = requests.get(URL)
+
+# soup = BeautifulSoup(response.text, 'html.parser')
+
+
+if len(sys.argv) == 3:
+    
+elif len(sys.argv) != 5:
+    
+    
+else:
+    print("Kein Argument übergeben.")
+    exit
---
+++ menu.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+function checkExists {
+    if ! [ -f /usr/share/dict/$1 ]
+    then
+        echo "Sie haben keinen ngerman in /usr/share/dict. Paket herunterladen w$1."
+        echo "Vielleicht versuchen zu laufen"
+        echo "sudo apt install w$1"
+        exit
+    fi
+}
+
+function menu {
+    echo "#### Wörterbuchhersteller"
+    echo "(1) Automatisch für ngerman herstellen"
+    echo "(2) Automatisch für ogerman herstellens"
+    echo "(3) Automatisch für ngerman und ogerman herstellen"
+    echo "(4) Herstellen für eine andere Datei an"
+    echo "(5) Verlassen"
+    read -p "Option eingeben: " option
+
+    case $option in 
+        1)
+            checkExists ngerman
+            ./hersteller.py /usr/share/dict/ngerman wb_ndeutsch
+            exit
+            ;;
+        2)
+            checkExists ogerman
+            ./hersteller.py /usr/share/dict/ogerman wb_odeutsch
+
+            exit
+            ;;
+        3)
+            checkExists ngerman
+            checkExists ogerman
+            ./hersteller.py /usr/share/dict/ngerman wb_ndeutsch /usr/share/dict/ogerman wb_odeutsch
+
+            exit
+            ;;
+        4)
+            read -p "Dateipfad eingeben: " dateipfad
+            if ! [ $dateipfad ]
+            then
+                echo "Keine solche Datei."
+                exit
+            fi
+            read -p "Benennen Sie die Wörterbuchdatei: " wdatei
+            ./hersteller.py $dateipfad $wdatei
+            exit
+            ;;    
+        
+        5)
+            exit
+            ;; 
+        *)
+            echo falsch
+            sleep 2
+            menu
+    esac
+
+}
+
+
+
+case $1 in
+    -n)
+        checkExists ngerman
+        ./hersteller.py /usr/share/dict/ngerman wb_ndeutsch
+        exit
+        ;;
+    -o)
+        checkExists ogerman
+        ./hersteller.py /usr/share/dict/ogerman wb_odeutsch
+        exit
+        ;;
+    -no)
+        checkExists ngerman
+        checkExists ogerman
+        ./hersteller.py /usr/share/dict/ngerman wb_ndeutsch /usr/share/dict/ogerman wb_odeutsch
+        exit
+        ;;
+    -on)
+        checkExists ngerman
+        checkExists ogerman
+        ./hersteller.py /usr/share/dict/ngerman wb_ndeutsch /usr/share/dict/ogerman wb_odeutsch
+        exit
+        ;;
+    -d)
+        read -p "Dateipfad eingeben: " dateipfad
+        if ! [ $dateipfad ]
+        then
+            echo "Keine solche Datei."
+            exit
+        fi
+        read -p "Benennen Sie die Wörterbuchdatei: " wdatei
+        ./hersteller.py $dateipfad $wdatei        
+        exit
+        ;;    
+    *)
+        menu
+esac
